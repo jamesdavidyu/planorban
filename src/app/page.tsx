a43189bae041e755dcba759e47d0472cb94916c4
You Young June <youngjune@planorban.com>
2025-02-14 03:13:41 -0500

    some updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,11 +34,14 @@ export default function Home() {
         <div>
           <a
             href="https://www.planorban.com"
-            className="flex w-fit"
+            className="flex w-fit -mt-2"
             title="Planorban"
           >
             <Avatar>
-              <AvatarImage src="https://m2my1rq4tt.ufs.sh/f/AR95GDbqfmiLSgxEf8i4ZGeSIWMpQRy6gK7A5vNl0HaJDrVx" />
+              <AvatarImage
+                src="https://m2my1rq4tt.ufs.sh/f/AR95GDbqfmiLSgxEf8i4ZGeSIWMpQRy6gK7A5vNl0HaJDrVx"
+                className={`${darkMode ? "invert" : ""}`}
+              />
             </Avatar>
           </a>
         </div>
@@ -69,7 +72,7 @@ export default function Home() {
                 <HoverCardTrigger
                   href="https://github.com/jamesdavidyu/"
                   target="_blank"
-                  className="underline hover:no-underline text-blue-500"
+                  className="underline hover:no-underline text-blue-600"
                 >
                   James Yu.
                 </HoverCardTrigger>
@@ -115,7 +118,7 @@ export default function Home() {
                   />
                   <img
                     src="https://utfs.io/f/18697db9-42df-4a31-b981-ababe7b32bb7-1xrfm.png"
-                    className="size-10"
+                    className="-mx-1 size-10"
                     title="Java"
                     alt="Java"
                   />
@@ -165,12 +168,60 @@ export default function Home() {
                     title="Next.js"
                     alt="Next.js"
                   />
+                  <img
+                    src="https://m2my1rq4tt.ufs.sh/f/e3892ea3-6d89-41d2-be4e-2496b69b5802-1sj3pb.png"
+                    className="size-10"
+                    title="React"
+                    alt="React"
+                  />
+                  <img
+                    src="https://m2my1rq4tt.ufs.sh/f/e1c5d774-9034-4d27-bac4-83609a2c0cf7-m0jdt1.png"
+                    className="size-10"
+                    title="Django"
+                    alt="Django"
+                  />
+                  <img
+                    src="https://m2my1rq4tt.ufs.sh/f/c89ce08c-0926-4b1f-bbc7-2e4306bab049-mj0jnj.png"
+                    className="-mx-2 w-12"
+                    title="Spring Boot"
+                    alt="Spring Boot"
+                  />
                 </MenubarContent>
               </MenubarMenu>
               <MenubarMenu>
                 <MenubarTrigger className="hover:cursor-pointer">
                   Cloud
                 </MenubarTrigger>
+                <MenubarContent
+                  className={`flex justify-center items-center gap-x-2 p-2 ${
+                    darkMode ? "bg-black text-white" : ""
+                  }`}
+                >
+                  <img
+                    src="https://m2my1rq4tt.ufs.sh/f/b590f013-c0b2-4166-8aef-f9430a4ca4fd-dk6j3t.png"
+                    className={`w-20 -mx-6 ${darkMode ? "" : "invert"}`}
+                    title="Vercel"
+                    alt="Vercel"
+                  />
+                  <img
+                    src="https://m2my1rq4tt.ufs.sh/f/72df50f8-3c93-4a65-8b7e-11f6c3e09312-26b7.svg"
+                    className={`size-10 ${darkMode ? "" : "invert"}`}
+                    title="Fly.io"
+                    alt="Fly.io"
+                  />
+                  <img
+                    src="https://m2my1rq4tt.ufs.sh/f/b2db0c49-4824-43b4-816d-094ab300115e-1pysc4.png"
+                    className="size-10"
+                    title="MongoDB"
+                    alt="MongoDB"
+                  />
+                  <img
+                    src="https://m2my1rq4tt.ufs.sh/f/e947aa1f-326e-4897-aa8f-9cb8bd70b916-22v1.webp"
+                    className={`w-10 -my-2 ${darkMode ? "" : "invert"}`}
+                    title="AWS"
+                    alt="AWS"
+                  />
+                </MenubarContent>
               </MenubarMenu>
             </Menubar>
           </div>
@@ -203,7 +254,7 @@ export default function Home() {
           </a>
           <Projects darkMode={darkMode} />
         </main>
-        <footer className="flex justify-end w-full border-t p-4 mt-8">
+        <footer className="flex justify-center 2xl:justify-end w-full border-t p-4 mt-8">
           <div className="flex items-center gap-x-2">
             <a
               href="https://github.com/jamesdavidyu/planorban"
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,3 +1,5 @@
+import { Avatar, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
 import { Card } from "./ui/card";
 
 interface ProjectsProps {
@@ -6,44 +8,117 @@ interface ProjectsProps {
 
 export const Projects = ({ darkMode }: ProjectsProps) => {
   return (
-    <div className="flex flex-col space-y-4">
+    <div className="flex flex-col items-center space-y-4">
       <div className="flex flex-col 2xl:flex-row gap-x-4 space-y-4 2xl:space-y-0">
-        <Card className={`${darkMode ? "bg-black text-white" : ""}`}>
+        <Card
+          className={`w-[475px] 2xl:w-[525px] ${
+            darkMode ? "bg-black text-white" : ""
+          }`}
+        >
           <a href="https://syraviews.com" target="_blank">
             <img
               src="https://m2my1rq4tt.ufs.sh/f/AR95GDbqfmiLFmBpIIul6K25DGPkXTZn7jetRmC0bSOw1Bxf"
-              className="h-72 rounded-t-xl"
+              className="h-72 rounded-t-xl border-b"
             />
           </a>
+          <div className="flex flex-col p-4">
+            <Button>syraviews.com</Button>
+            <p className="mt-3"></p>
+          </div>
         </Card>
-        <Card className={`${darkMode ? "bg-black text-white" : ""}`}>
+        <Card
+          className={`w-[475px] 2xl:w-[525px] ${
+            darkMode ? "bg-black text-white" : ""
+          }`}
+        >
           <a href="https://syraqs.com" target="_blank">
             <img
               src="https://m2my1rq4tt.ufs.sh/f/AR95GDbqfmiLnLmDusdqectlQXLhk6oKRjmaTi8nwNMzuGYf"
-              className="h-72 rounded-t-xl"
+              className="h-72 rounded-t-xl border-b"
             />
           </a>
+          <div className="flex flex-col p-4">
+            <Button>syraqs.com</Button>
+            <p className="mt-3"></p>
+          </div>
         </Card>
       </div>
-      <div className="flex gap-x-4">
-        <Card className={`${darkMode ? "bg-black text-white" : ""}`}>
+      <div className="flex flex-col items-center 2xl:flex-row gap-x-4 space-y-4 2xl:space-y-0">
+        <Card
+          className={`w-[475px] 2xl:w-[525px] ${
+            darkMode ? "bg-black text-white" : ""
+          }`}
+        >
           <a href="https://youyoungjune.com" target="_blank">
             <img
               src="https://m2my1rq4tt.ufs.sh/f/AR95GDbqfmiLWwroDiBjUVQNz20JR5M8ohxsYgOb7amnLw1B"
-              className="h-72 rounded-t-xl"
+              className="h-72 rounded-t-xl border-b"
             />
           </a>
+          <div className="flex flex-col p-4">
+            <Button>youyoungjune.com</Button>
+            <div className="mt-3">
+              <p className="italic text-green-600">
+                &#47;&#42;&nbsp;In Progress&nbsp;&#42;&#47;
+              </p>
+              My arts portfolio with JWT authentication &#40;users need to log
+              in to see the content&#41;, written in Typescript
+              &#40;Next.js&#41; and Java &#40;Spring Boot&#41; with MongoDB
+              &#40;Atlas&#41;.
+            </div>
+            <div className="flex justify-between mt-2">
+              <div className="flex items-center gap-x-2">
+                <Avatar>
+                  <a
+                    href="https://github.com/jamesdavidyu/planorban"
+                    target="_blank"
+                  >
+                    <AvatarImage src="https://m2my1rq4tt.ufs.sh/f/f9154ab4-83d6-4f30-8828-36ef21c11531-klmal9.png" />
+                  </a>
+                </Avatar>
+                <Button variant="destructive">Frontend Repo</Button>
+              </div>
+              <div className="flex items-center gap-x-2">
+                <Button variant="secondary">
+                  <a
+                    href="https://github.com/jamesdavidyu/yyjApi_java"
+                    target="_blank"
+                  >
+                    Backend Repo
+                  </a>
+                </Button>
+                <Avatar>
+                  <a
+                    href="https://github.com/jamesdavidyu/yyjApi_java"
+                    target="_blank"
+                  >
+                    <AvatarImage src="https://m2my1rq4tt.ufs.sh/f/f9154ab4-83d6-4f30-8828-36ef21c11531-klmal9.png" />
+                  </a>
+                </Avatar>
+              </div>
+            </div>
+          </div>
         </Card>
-        {/* <Card className={`${darkMode ? "bg-black text-white" : ""}`}>
-          <img
-            src={
-              darkMode
-                ? "https://m2my1rq4tt.ufs.sh/f/AR95GDbqfmiL5MnRTn8fLieHIWtKpZ0mgDCXwsE51NrnTlS4"
-                : "https://m2my1rq4tt.ufs.sh/f/AR95GDbqfmiLz9T58VEwfIZTOd5vKqxuLcyDSkljEV8JNaXH"
-            }
-            className="h-72 rounded-t-xl"
-          />
-        </Card> */}
+        <Card
+          className={`w-[475px] 2xl:w-[525px] ${
+            darkMode ? "bg-black text-white" : ""
+          }`}
+        >
+          <a href="https://github.com/jamesdavidyu" target="_blank">
+            <img
+              src={
+                darkMode
+                  ? "https://m2my1rq4tt.ufs.sh/f/AR95GDbqfmiL5MnRTn8fLieHIWtKpZ0mgDCXwsE51NrnTlS4"
+                  : "https://m2my1rq4tt.ufs.sh/f/AR95GDbqfmiLz9T58VEwfIZTOd5vKqxuLcyDSkljEV8JNaXH"
+              }
+              className="h-72 rounded-t-xl border-b"
+            />
+          </a>
+          <div className="flex flex-col p-4">
+            <Button>github.com/jamesdavidyu</Button>
+            <p className="mt-3">More Coming Soon!</p>
+          </div>
+        </Card>
       </div>
     </div>
   );
