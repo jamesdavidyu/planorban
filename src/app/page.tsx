96cfe66a098c2ea7a59e1aefabbaf64b4023c750
You Young June <youngjune@planorban.com>
2025-02-24 03:46:57 -0500

    updating link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,6 @@ export default function Home() {
         <div>
           <a
             href="https://planorban.com"
-            target="_blank"
             className="flex w-fit -mt-1"
             title="Planorban"
           >
